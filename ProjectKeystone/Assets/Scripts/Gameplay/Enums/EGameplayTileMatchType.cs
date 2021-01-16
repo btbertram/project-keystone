e64db12f00083f8c3aab8fcf033debc9872eaba2
btbertram <brennantbertram@gmail.com>
2021-01-16 00:00:01 -0800

    Initial File Structure + Tile Object
    Added:
    
    -New Folders
    --Scripts
    ---Gameplay
    ---Enums
    ---Controllers
    
    -New Gameplay Scripts
    --gameplayTile.cs
    ---Core Gameplay object which represents the main puzzle object that the player manipulates.
    ---func void TileReroll() : Randomly generates a new EGameplayTileMatchType for self.
    ---bool isTileActive : Denotes if the tile is cleared.
    
    --puzzleState.cs
    ---Gameplay object which tracks the state of the current puzzle.
    ---static System.Random rand : A member variable used to generate tiles.
    
    -New Gameplay Enums
    --EGameplayTileMatchType
    ---Enum meant to help identify which tiles can "match" when placed near each other in a grid.

---
+++ ProjectKeystone/Assets/Scripts/Gameplay/Enums/EGameplayTileMatchType.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum EGameplayTileMatchType
+{
+    error,
+    square,
+    circle,
+    tri,
+    hex,
+    star,
+    diamond,
+    special
+}
---
+++ ProjectKeystone/Assets/Scripts/Gameplay/Objects/gameplayTile.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class gameplayTile : MonoBehaviour
+{
+
+    public EGameplayTileMatchType tileMatchType;
+    public bool isTileActive;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        isTileActive = true;
+        TileMatchTypeReroll();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void TileMatchTypeReroll()
+    {
+        tileMatchType = (EGameplayTileMatchType)puzzleState.rand.Next((int)EGameplayTileMatchType.square, (int)EGameplayTileMatchType.special);
+    }
+
+
+
+}
