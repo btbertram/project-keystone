cbcc1960f5fad5e66d4455248aee053c29c97278
btbertram <brennantbertram@gmail.com>
2021-02-10 18:07:01 -0800

    Prototype Presenter Classes
    -Project Navigation
    --Added Presenters folder in Scripts
    --Added Sprites folder in Assets
    
    -New Presenter Classes: These classes control unity gameObjects for visuals for their associated backend object.
    --TilePresenter
    --CursorPresenter
    --GridPresenter
    ---GenerateGridSprites function wip
    --GameObjectPresenter: Presenter manager object.
    
    -Project Assets
    --GridLine.png/prefab: Sprite for the gridlines.

---
+++ ProjectKeystone/Assets/Scripts/Presenters/GridPresenter.cs
@@ -0,0 +1,62 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/// <summary>
+/// A MonoBehaviour Class that handles and displays the visual components of the PuzzleGrid data class.
+/// </summary>
+public class GridPresenter : MonoBehaviour
+{
+    public int gridSpacing;
+    public float lineThickness;
+    public GameObject GridLinePrefab;
+    // Start is called before the first frame update
+    void Start()
+    {
+        PuzzleGameObject puzzle = GameObject.FindObjectOfType<PuzzleGameObject>();
+
+        GenerateGridSprites(puzzle.puzzleSizeX, puzzle.puzzleSizeY);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    /// <summary>
+    /// Creates a visual representation of the Grid.
+    /// </summary>
+    public void GenerateGridSprites(int puzzleSizeX, int puzzleSizeY)
+    {
+        Vector3 pos = Vector3.zero;
+        Vector3 scale = Vector3.zero;
+        Quaternion quaternion = Quaternion.identity;
+        Quaternion rotation = Quaternion.Euler(0, 0, 90);
+
+        Vector3 lineScaleY = new Vector3(lineThickness, puzzleSizeX * gridSpacing, 0);
+        Vector3 lineScaleX = new Vector3(lineThickness, puzzleSizeY * gridSpacing, 0);
+
+
+        for (int y = 0; y < puzzleSizeY+1; y++)
+        {
+            pos.y = (gridSpacing * y);
+            GameObject gameObject = Instantiate(GridLinePrefab, pos, rotation);
+            gameObject.transform.localScale = lineScaleY;
+        }
+
+        //Clear Vector
+        pos = Vector3.zero;
+
+        for (int x = 0; x < puzzleSizeX+1; x++)
+        {
+            pos.x = (gridSpacing * x);
+            GameObject gameObject = Instantiate(GridLinePrefab, pos, quaternion);
+            gameObject.transform.localScale = lineScaleX;
+        }
+
+        //Instance new sprite
+        //Multiply scale by puzzle size
+        //Position based on loop number
+    }
+}
